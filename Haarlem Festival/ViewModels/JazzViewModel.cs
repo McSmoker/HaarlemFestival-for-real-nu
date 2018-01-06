c1796695f3a5d4e8557ad85627e63fbc97b072f0
Neorath <j_vdbulk@hotmail.com>
2018-01-06 21:41:38 +0100

    Cleaned solution. Jazz javascript naar jquery omgezet. Talking layout gegeven. Jazzviewmodel aangepast.

--- Haarlem Festival/ViewModels/JazzViewModel.cs
+++ Haarlem Festival/ViewModels/JazzViewModel.cs
@@ -8,14 +8,14 @@ namespace Haarlem_Festival.ViewModels
 {
     public class JazzViewModel
     {
-        public int EventId { get; set; }
-        public DateTime EventStart { get; set; }
-        public DateTime EventEnd { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventTimeStart { get; set; }
+        public string EventTimeEnd { get; set; }
         public string Location { get; set; }
+        public string Hall { get; set; }
         public int Seats { get; set; }
         public int TicketsSold { get; set; }
         public Performer Artist { get; set; }
-        public string Hall { get; set; }
-        public int PerformerId { get; set; }
+        public double Price { get; set; }
     }
 }
